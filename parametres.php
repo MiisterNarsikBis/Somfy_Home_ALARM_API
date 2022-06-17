1daa776744efb78e1c2bbe0e5ae0b9c5a8f77248
MiisterNarsik <mauriacoux.martin@gmail.com>
2022-06-18 00:45:56 +0200

    Missing parametre ?

--- parametres.php
+++ parametres.php
@@ -1,26 +1,23 @@
 <?php
-session_start();
 
 $log_level = 1;
-$config = parse_ini_file('../private/config.ini');
+$config = parse_ini_file('private/config.ini');
 $username = $config['username'];
 $password = $config['password'];
 $client_id = $config['client_id'];
 $client_secret = $config['client_secret'];
-$access_token = $_SESSION["access_token"];
-$refresh_token = $_SESSION["refresh_token"];
+$access_token = isset($_SESSION["access_token"]) ? $_SESSION["access_token"] : null;
+$refresh_token = isset($_SESSION["refresh_token"]) ? $_SESSION["refresh_token"] : null;
 
-
-if ($_SESSION["site_id"] == ""){
-$infos_user = "https://api.myfox.io/v3/user?access_token=".$access_token;
-$details_user = json_decode(file_get_contents($infos_user));
-$delais_activation = ($details_user->sites[0]->exit_delay)+5; #delais d'activation de votre alarme +5 secs
-$_SESSION["site_id"] = $details_user->sites[0]->site_id;
+if ($_SESSION["site_id"] == null && $access_token != null){
+        $infos_user = "https://api.myfox.io/v3/user?access_token=".$access_token;
+        $details_user = json_decode(file_get_contents($infos_user));
+        $delais_activation = ($details_user->sites[0]->exit_delay)+5; #delais d'activation de votre alarme +5 secs
+        $_SESSION["site_id"] = $details_user->sites[0]->site_id;
 }
-$site_id = $_SESSION["site_id"];
+$site_id = isset($_SESSION["site_id"]) ? $_SESSION["site_id"] : null;
 
-
-if ($_SESSION["device_id"] == ""){
+if (isset($_SESSION["device_id"]) && $_SESSION["device_id"] == null && $access_token != null){
 $infos_devices = "https://api.myfox.io/v3/site/".$site_id."/device?access_token=".$access_token;
 $details_devices = json_decode(file_get_contents($infos_devices));
         foreach($details_devices->items as $item)
